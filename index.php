14d63bd28bb523d3001802df2949f443f6a48c32
Gerhard Popp <gerhrad@poppgerhard.at>
2020-10-28 12:51:20 +0100

    Erste Artikel auf Startseite neu

--- index.php
+++ index.php
@@ -3,29 +3,64 @@
  * The default single page template.
  *
  * @author Freeshifter LLC
- * @since 1.0.0
+ * @since  1.0.0
  */
 
 namespace immobilien_redaktion_2020;
 
-get_header(); ?>
-<!--	<div class="container mx-auto relative z-10 mb-16 lg:mb-32 flex flex-wrap items-start">-->
-<!--		<div class="w-full lg:w-4/6 lg:pr-10 mb-8">-->
-<!--			--><?php
+get_header();
+
+$query = new \WP_Query([
+    'post_type'           => 'post',
+    'post_status'         => 'publish',
+    'ignore_sticky_posts' => true,
+    'posts_per_page'      => 2,
+    'category__not_in'    => [17, 696],
+    'tag__not_in'         => 989,
+]);
+?>
+
+    <div class="container mx-auto mt-32">
+        <div class="grid grid-cols-2 gap-10">
+            <?php if ($query->have_posts()): ?>
+                <?php while ($query->have_posts()): ?>
+                    <?php $query->the_post(); ?>
+                    <div class="col-span-2 md:col-span-1 relative">
+                        <a href="<?php the_permalink(); ?>"class="relative block">
+                            <?php the_post_thumbnail('article', ['class' => 'w-full h-auto max-w-full']); ?>
+                        <div class="absolute bottom-0 left-0 m-5">
+                            <h1 class="font-serif text-white text-2xl"><?php the_title() ?></h1>
+                        </div>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+        <?php wp_reset_postdata(); ?>
+    </div>
+
+
+    <!--	<div class="container mx-auto relative z-10 mb-16 lg:mb-32 flex flex-wrap items-start">-->
+    <!--		<div class="w-full lg:w-4/6 lg:pr-10 mb-8">-->
+    <!--			--><?php
 //			if ( have_posts() ) {
 //				while ( have_posts() ) {
 //					the_post();
 //					echo get_template_part( 'content-templates/content', 'article' );
 //				}
-//			} ?>
-<!--		</div>-->
-<!--		--><?php
-//		if ( is_active_sidebar( 'sidebar' ) ) : ?>
-<!--			<aside class="w-full lg:w-2/6 bg-white border-gray-400 border-2 p-8">-->
-<!--				--><?php //dynamic_sidebar( 'sidebar' ); ?>
-<!--			</aside>-->
-<!--		--><?php
-//		endif; ?>
-<!--	</div>-->
-	<?php
+//			}
+?>
+    <!--		</div>-->
+    <!--		--><?php
+//		if ( is_active_sidebar( 'sidebar' ) ) :
+?>
+    <!--			<aside class="w-full lg:w-2/6 bg-white border-gray-400 border-2 p-8">-->
+    <!--				--><?php //dynamic_sidebar( 'sidebar' );
+?>
+    <!--			</aside>-->
+    <!--		--><?php
+//		endif;
+?>
+    <!--	</div>-->
+    <?php
 get_footer();
--- src/setup.php
+++ src/setup.php
@@ -6,32 +6,42 @@ namespace immobilien_redaktion_2020;
  * Set up theme defaults and registers support for various WordPress features.
  *
  * @author Freeshifter LLC
- * @since 1.0.0
+ * @since  1.0.0
  */
-add_action( 'after_setup_theme', function () {
+add_action('after_setup_theme', function () {
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support('automatic-feed-links');
 
-	// Let WordPress manage the document title.
-	add_theme_support( 'title-tag' );
+    // Let WordPress manage the document title.
+    add_theme_support('title-tag');
 
-	// Enable support for Post Thumbnails on posts and pages.
-	add_theme_support( 'post-thumbnails' );
+    // Enable support for Post Thumbnails on posts and pages.
+    add_theme_support('post-thumbnails');
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( [
-		'primary' => __( 'Primary Menu', 'immobilien-redaktion-2020' ),
-		'footer'  => __( 'Footer Menu', 'immobilien-redaktion-2020' ),
-	] );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus([
+        'primary' => __('Primary Menu', 'immobilien-redaktion-2020'),
+        'footer'  => __('Footer Menu', 'immobilien-redaktion-2020'),
+    ]);
 
-	// Switch default core markup for search form, comment form, and comments to output valid HTML5.
-	add_theme_support( 'html5', [
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	] );
-	
-} );
+    // Switch default core markup for search form, comment form, and comments to output valid HTML5.
+    add_theme_support('html5', [
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ]);
+
+
+    add_image_size('custom-thumbnail', 800, 600, true);
+    add_image_size('featured', 800, 450, true);
+    add_image_size('featured_small', 300, (300 / 16) * 9, true);
+    add_image_size('author_small', 48, 48, true);
+    add_image_size('horizontal_box', 370, 265, true);
+    add_image_size('article', 600, 450, true);
+    add_image_size('portrait', 300);
+
+
+});
