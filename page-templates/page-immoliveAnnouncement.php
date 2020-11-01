625a7b8073d2ae53b24d063fa0ef74a8eb78823e
Gerhard Popp <gerhrad@poppgerhard.at>
2020-11-01 12:00:07 +0100

    updates immolive announcement

--- page-templates/page-immoliveAnnouncement.php
+++ page-templates/page-immoliveAnnouncement.php
@@ -27,17 +27,17 @@ if ($query->have_posts()):
              style="background-image: url(<?php echo get_the_post_thumbnail_url() ?>);
                      background-size: cover;
                      background-position: center">
-            <div class="absolute w-full h-full top-0 left-0 bg-black bg-opacity-25"></div>
-            <div class="my-16">
+            <div class="absolute w-full h-full top-0 left-0 bg-black bg-opacity-25 z-10"></div>
+            <div class="my-16 z-20">
                 <h1 class="bg-white text-2xl px-5 text-center inline font-semibold"><?php the_title() ?></h1>
             </div>
-            <div class="my-16">
+            <div class="my-16 z-20">
                 <h1 class="bg-white text-2xl px-5 text-center inline font-semibold">Ein
                     <span class="font-serif uppercase">Immo</span>
                     <span class="font-serif text-primary-100 uppercase">Live</span> am
                     <?php echo \Carbon\Carbon::parse(get_field('field_5ed527e9c2279'))->format('d.m.Y H:i') ?> Uhr </h1>
             </div>
-            <div class="mb-32">
+            <div class="mb-32 z-20">
                 <a href="<?php the_permalink(); ?>" class="bg-primary-100 px-5 lg:text-center text-white text-2xl py-2">
                     Jetzt Anmelden
                 </a>
