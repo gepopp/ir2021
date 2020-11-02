f72d90882f091915702af665a5faf35a708bb1e2
Gerhard Popp <gerhrad@poppgerhard.at>
2020-11-02 22:11:42 +0100

    DEBUG slider in den videokategorien auf sehen

--- pagetemplate-sehen.php
+++ pagetemplate-sehen.php
@@ -274,9 +274,9 @@ foreach ($cats as $cat): ?>
                 </template>
             </div>
             <div class="flex items-center justify-between flex-1 absolute top-0 w-full h-full">
-                <button class="outline-none focus:outline-none rounded-full mx-4 text-white w-8 h-8 p-2 rounded-full bg-gray-900 flex items-center justify-center"
+                <button class="outline-none focus:outline-none rounded-full mx-4 text-white"
                         x-on:click="prev($refs);">
-                    <div x-show="active > 0">
+                    <div x-show="active > 0" class="w-8 h-8 p-2 rounded-full bg-gray-900 flex items-center justify-center">
                         <svg class="w-6 h-6 text-white inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
                         </svg>
@@ -293,9 +293,9 @@ foreach ($cats as $cat): ?>
                 <?php endif; ?>
 
                 <button
-                        class="outline-none focus:outline-none rounded-full mx-4 text-white w-8 h-8 p-2 rounded-full bg-gray-900 flex items-center justify-center"
+                        class="outline-none focus:outline-none rounded-full mx-4 text-white"
                         x-on:click="next($refs);">
-                    <div x-show="active <= pages">
+                    <div x-show="active <= pages" class="w-8 h-8 p-2 rounded-full bg-gray-900 flex items-center justify-center">
                         <svg class="w-6 h-6 text-white inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                             <path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
