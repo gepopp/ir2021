4a7b14dc3636e8b5ae017ac3c2dfe400e4225825
Gerhard Popp <gerhrad@poppgerhard.at>
2020-11-05 11:05:53 +0100

    debug menu categorying

--- single.php
+++ single.php
@@ -9,15 +9,23 @@ $cat = get_category($cat)
 ?>
 <?php if (!empty(get_field('field_5ded37c474589', 'user_' . get_the_author_meta('ID'))['sizes']['xs'])
     && checkRemoteFile(get_field('field_5ded37c474589', 'user_' . get_the_author_meta('ID'))['sizes']['xs'])
-    && !empty(get_field(get_the_author_meta('description')))
+    && !empty(get_field(get_the_author_meta('description')))): ?>
 
-): ?>
     <div class="conatainer mx-auto mt-32 flex justify-center items-center">
         <div class="flex justify-center items-center">
             <img src="<?php echo get_field('field_5ded37c474589', 'user_' . get_the_author_meta('ID'))['sizes']['author_small'] ?>" class="rounded-full w-12 h-12">
             <p class="ml-5 text-xl underline"><?php echo get_the_author_posts_link(get_the_ID()) ?></p>
         </div>
     </div>
+
+<?php else: ?>
+
+    <div class="conatainer mx-auto mt-32 flex justify-center items-center">
+        <div class="flex justify-center items-center">
+            <p class="ml-5 text-xl">Geschrieben von <?php echo get_the_author_meta('display_name')?></p>
+        </div>
+    </div>
+
 <?php endif; ?>
 
     <div class="container mx-auto mt-32">
@@ -133,7 +141,7 @@ $cat = get_category($cat)
 
                     </div>
 
-                    <?php echo preg_replace('#\[[^\]]+\]#', '',get_the_content()); ?>
+                    <?php echo preg_replace('#\[[^\]]+\]#', '', get_the_content()); ?>
                 </div>
             </div>
 
