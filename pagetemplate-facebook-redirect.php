ec113353f90595dbf586a0552f0cced7c52c9ee0
Gerhard Popp <gerhrad@poppgerhard.at>
2020-11-16 13:07:53 +0100

    fb oauth

--- pagetemplate-facebook-redirect.php
+++ pagetemplate-facebook-redirect.php
@@ -30,6 +30,21 @@ if(count($name) > 1){
 }
 $lastname = implode(' ', $name);
 
+if(isset($_GET['login'])){
+    $user = get_user_by('email', $email);
+    if ( !is_wp_error( $user ) )
+    {
+        wp_clear_auth_cookie();
+        wp_set_current_user ( $user->ID );
+        wp_set_auth_cookie  ( $user->ID );
+
+
+        wp_safe_redirect( home_url('profil') );
+        exit();
+    }
+}
+
+
 $_SESSION['register_fristname'] = $firstname;
 $_SESSION['register_lastname'] = $lastname;
 $_SESSION['register_email'] = $email;
