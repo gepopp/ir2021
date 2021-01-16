e0b4e116a9b86b4e7e2f2ef2d084d87925268313
Gerhard Popp <gerhrad@poppgerhard.at>
2021-01-16 14:43:33 +0100

    excerpt semipold

--- page-templates/page-immoliveAnnouncement.php
+++ page-templates/page-immoliveAnnouncement.php
@@ -23,26 +23,28 @@ if ($query->have_posts()):
         $query->the_post();
         ?>
 
-        <div class="container mx-auto mt-20 border-15 flex flex-col items-center justify-center relative" style="height: 512px;  background-image: url(<?php echo get_the_post_thumbnail_url() ?>);
+        <div class="container mx-auto mt-20 border-15 flex flex-col justify-end relative" style="height: 512px;  background-image: url(<?php echo get_the_post_thumbnail_url() ?>);
                 background-size: cover;
                 background-position: center">
-            <div class="flex flex-col justify-center text-center">
-                <h1 class="bg-white text-2xl px-5 text-center inline font-semibold"><?php the_title() ?></h1>
-                <h1 class="bg-white text-2xl px-5 text-center inline font-semibold">Ein
+            <div class="flex justify-between w-full bg-white p-5">
+                <div>
+                <p class="bg-white text-xl  lg:text-2xl px-5 font-semibold"><?php the_title() ?></p>
+                <p class="bg-white text-sm lg:text-xl px-5 font-semibold">Ein
                     <span class="font-serif uppercase">Immo</span>
                     <span class="font-serif text-primary-100 uppercase">Live</span> am
-                    <?php echo \Carbon\Carbon::parse(get_field('field_5ed527e9c2279'))->format('d.m.Y H:i') ?> Uhr </h1>
+                    <?php echo \Carbon\Carbon::parse(get_field('field_5ed527e9c2279'))->format('d.m.Y H:i') ?> Uhr </p>
+                </div>
 
                 <?php if (!empty(get_field('field_5ed52801c227a'))): ?>
-                    <div class="mt-10">
-                        <a href="<?php the_field('field_5ed52801c227a') ?>" class="bg-primary-100 px-5 lg:text-center text-white text-2xl py-2">
+                    <div class="flex items-center">
+                        <a href="<?php the_field('field_5ed52801c227a') ?>" class="bg-primary-100 px-5 lg:text-center text-white text-lg lg:text-2xl py-2 whitespace-no-wrap">
                             Jetzt Anmelden
                         </a>
                     </div>
                 <?php endif; ?>
             </div>
 
-            <div class="absolute bottom-0 right-0 m-6">
+            <div class="absolute top-0 right-0 m-6">
                 <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/poweredbygoreeo.png" class="w-24 h-24">
             </div>
         </div>
