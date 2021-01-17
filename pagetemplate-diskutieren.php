3d6bb9378d27dd7abb54f625c088af5d80c7f70f
Gerhard Popp <gerhrad@poppgerhard.at>
2021-01-17 13:23:41 +0100

    speed up loading video preview thumbnail

--- pagetemplate-diskutieren.php
+++ pagetemplate-diskutieren.php
@@ -32,7 +32,42 @@ $count = $query->post_count;
 if ($query->have_posts()):
     $runner = 1;
     while ($query->have_posts()):
-        $query->the_post();
+        $query->the_post(); ?>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Event",
+      "name": "<?php the_title() ?>",
+      "startDate": "<?php echo \Carbon\Carbon::parse(get_field('field_5ed527e9c2279'))->format('c') ?>",
+      "endDate": "<?php echo \Carbon\Carbon::parse(get_field('field_5ed527e9c2279'))->addHour()->format('c') ?>",
+      "eventStatus": "https://schema.org/EventScheduled",
+      "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode",
+      "location": {
+        "@type": "VirtualLocation",
+        "url": "<?php echo home_url('diskutieren') ?>"
+        },
+      "image": [
+        "<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full') ?>",
+        "<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium') ?>",
+        "<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail') ?>"
+       ],
+      "description": "<?php echo get_the_content() ?>",
+      "offers": {
+        "@type": "Offer",
+        "url": "<?php echo get_field('field_5ed52801c227a') ?>",
+        "price": "0",
+        "priceCurrency": "EUR",
+        "availability": "https://schema.org/InStock",
+        "validFrom": "<?php echo the_time('c') ?>"
+      },
+      "organizer": {
+        "@type": "Organization",
+        "name": "Die unabhängige Immoblilien Redaktion",
+        "url": "<?php echo home_url() ?>"
+      }
+    }
+    </script>
+    <?php
         if ($runner == 1):
             ?>
             <div class="lg:h-screen-75 flex lg:-mx-5" x-data="counter('<?php the_field('field_5ed527e9c2279') ?>')" x-init="count()">
