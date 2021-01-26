dca248044799c7708c6fe174744e29e55b180a8f
Gerhard Popp <gerhrad@poppgerhard.at>
2021-01-26 18:22:26 +0100

    purge css update

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,6 @@ if ( isProduction ) {
 				'./*.php',
 				'./src/**/*.php',
 				'./page-templates/*.php',
-				'./pagetemplate-diskutieren.php',
-				'./index.php',
 				'./banner-templates/*.php',
 				'./assets/images/**/*.svg',
 				'./assets/scss/whitelist/*.css',
@@ -208,6 +206,8 @@ function getCSSWhitelistPatterns() {
 		/^figure(-.*)?$/,
 		/^figcaption(-.*)?$/,
 		/^ol(-.*)?$/,
+		/^xl(:.*)?$/,
+		/^lg(:.*)?$/,
 	];
 }
 
