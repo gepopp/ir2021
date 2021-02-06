c544132f2564036798069625e6682fed3b60eedd
Gerhard Popp <gerhrad@poppgerhard.at>
2021-02-06 12:09:30 +0100

    ads acf in theme && updates menu with walker

--- pagetemplate-diskutieren.php
+++ pagetemplate-diskutieren.php
@@ -160,25 +160,25 @@ if ($query->have_posts()):
                                                 E-Mail login
                                             </a>
                                         </div>
-                                        <div class="col-span-2 xl:col-span-1">
-                                            <?php
-                                            $config = [
-                                                'facebook' => [
-                                                    'client_id'     => '831950683917414',
-                                                    'client_secret' => 'd6d52d59ce1f1efdbf997b980dffe229',
-                                                    'redirect'      => home_url('fb-login'),
-                                                ],
-                                            ];
-
-                                            $socialite = new SocialiteManager($config);
-                                            ?>
-
-                                            <a href="<?php echo $socialite->create('facebook')->redirect(); ?>"
-                                               class="block bg-white text-primary-100 border border-primary-100 font-semibold text-center shadow-xl py-3 my-5 text-lg focus:outline-none focus:shadow-outline w-full text-center cursor-pointer"
-                                            >
-                                                Facebook login
-                                            </a>
-                                        </div>
+<!--                                        <div class="col-span-2 xl:col-span-1">-->
+<!--                                            --><?php
+//                                            $config = [
+//                                                'facebook' => [
+//                                                    'client_id'     => '831950683917414',
+//                                                    'client_secret' => 'd6d52d59ce1f1efdbf997b980dffe229',
+//                                                    'redirect'      => get_field(),
+//                                                ],
+//                                            ];
+//
+//                                            $socialite = new SocialiteManager($config);
+//                                            ?>
+<!---->
+<!--                                            <a href="--><?php //echo $socialite->create('facebook')->redirect(); ?><!--"-->
+<!--                                               class="block bg-white text-primary-100 border border-primary-100 font-semibold text-center shadow-xl py-3 my-5 text-lg focus:outline-none focus:shadow-outline w-full text-center cursor-pointer"-->
+<!--                                            >-->
+<!--                                                Facebook login-->
+<!--                                            </a>-->
+<!--                                        </div>-->
                                     </div>
                                 </div>
                             </div>
@@ -313,25 +313,25 @@ if ($query->have_posts()):
                                                             E-Mail login
                                                         </a>
                                                     </div>
-                                                    <div class="col-span-2 xl:col-span-1">
-                                                        <?php
-                                                        $config = [
-                                                            'facebook' => [
-                                                                'client_id'     => '831950683917414',
-                                                                'client_secret' => 'd6d52d59ce1f1efdbf997b980dffe229',
-                                                                'redirect'      => home_url('fb-login'),
-                                                            ],
-                                                        ];
-
-                                                        $socialite = new SocialiteManager($config);
-                                                        ?>
-
-                                                        <a href="<?php echo $socialite->create('facebook')->redirect(); ?>"
-                                                           class="block bg-white text-primary-100 border border-primary-100 font-semibold text-center shadow-xl py-3 my-5 text-lg focus:outline-none focus:shadow-outline w-full text-center cursor-pointer"
-                                                        >
-                                                            Facebook login
-                                                        </a>
-                                                    </div>
+<!--                                                    <div class="col-span-2 xl:col-span-1">-->
+<!--                                                        --><?php
+//                                                        $config = [
+//                                                            'facebook' => [
+//                                                                'client_id'     => '831950683917414',
+//                                                                'client_secret' => 'd6d52d59ce1f1efdbf997b980dffe229',
+//                                                                'redirect'      => home_url('fb-login'),
+//                                                            ],
+//                                                        ];
+//
+//                                                        $socialite = new SocialiteManager($config);
+//                                                        ?>
+<!---->
+<!--                                                        <a href="--><?php //echo $socialite->create('facebook')->redirect(); ?><!--"-->
+<!--                                                           class="block bg-white text-primary-100 border border-primary-100 font-semibold text-center shadow-xl py-3 my-5 text-lg focus:outline-none focus:shadow-outline w-full text-center cursor-pointer"-->
+<!--                                                        >-->
+<!--                                                            Facebook login-->
+<!--                                                        </a>-->
+<!--                                                    </div>-->
                                                 </div>
 
                                             </div>
