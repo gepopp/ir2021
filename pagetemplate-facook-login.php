ca34829d9992329fa22a3e2aed02ad134711658c
Gerhard Popp <gerhrad@poppgerhard.at>
2021-02-06 16:49:52 +0100

    ads acf in theme && updates menu with walker

--- pagetemplate-facook-login.php
+++ pagetemplate-facook-login.php
@@ -35,7 +35,7 @@ if ( $user ) {
     wp_clear_auth_cookie();
     wp_set_current_user($user->ID);
     wp_set_auth_cookie($user->ID);
-    wp_safe_redirect(home_url('profil'));
+    wp_safe_redirect(isset($_GET['goto']) ? $_GET['goto']: home_url('profil'));
     exit();
 }else{
     $_SESSION['login_error'] = 'Wir konnten Sie mit dieser E-Mail Adresse nicht einloggen.';
