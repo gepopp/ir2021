e6cdd65d26d3bf97ef60718f40ac59bd02487185
Gerhard Popp <gerhrad@poppgerhard.at>
2021-02-06 21:52:24 +0100

    debug

--- pagetemplate-facook-login.php
+++ pagetemplate-facook-login.php
@@ -54,9 +54,7 @@ wp_clear_auth_cookie();
 wp_set_current_user($user->ID);
 wp_set_auth_cookie($user->ID);
 
-wp_redirect(home_url());
+$redirect = !empty($_GET['state']) ? base64_decode($_GET['state']) : get_field('field_601bc4580a4fc', 'option');
+wp_safe_redirect($redirect);
 die();
-
-
-wp_safe_redirect(isset($_GET['state']) ? $_GET['state'] : get_field('field_601bc4580a4fc', 'option'));
 
