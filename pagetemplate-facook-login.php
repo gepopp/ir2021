680829761f4f8ae7c14e74fde3396369a19611a5
Gerhard Popp <gerhrad@poppgerhard.at>
2021-02-06 21:44:17 +0100

    debug

--- pagetemplate-facook-login.php
+++ pagetemplate-facook-login.php
@@ -21,7 +21,7 @@ $code = $_GET['code'];
 
 $user = $socialite->create('facebook')->userFromCode($code);
 
-wp_die(var_dump($user));
+wp_die(var_dump($_GET));
 
 $name = $user->getName();      // "安正超"
 $email = $user->getEmail();     // "anzhengchao@gmail.com"
