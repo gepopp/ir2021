fd61b26569a4a06ffa45d26904bc6c06fee20025
Gerhard Popp <gerhrad@poppgerhard.at>
2021-02-10 21:03:28 +0100

    logout post form - delete cookie

--- page-templates/login-form.php
+++ page-templates/login-form.php
@@ -67,11 +67,11 @@ $config = [
         'client_secret' => 'd6d52d59ce1f1efdbf997b980dffe229',
         'redirect'      => home_url('fb-login'),
     ],
-    'google' => [
+    'google'   => [
         'client_id'     => '194317471061-jdtvke2dpcensj3p9ckfq20cbsre23dl.apps.googleusercontent.com',
         'client_secret' => 'O_JXIOXqatwxOMYq45ggJ1tj',
         'redirect'      => home_url('g-oauth'),
-    ]
+    ],
 ];
 // 194317471061-jdtvke2dpcensj3p9ckfq20cbsre23dl.apps.googleusercontent.com
 // O_JXIOXqatwxOMYq45ggJ1tj
@@ -82,20 +82,39 @@ $socialite = new SocialiteManager($config);
     <hr class="my-4">
     <div class="my-5 w-full">
         <a href="<?php echo $socialite->create('facebook')->redirect(); ?>"
-           class="bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
+           class="flex justify-center items-center bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
         >
-            <?php _e('Via Facebook einloggen', 'ir21') ?>
+            <svg version="1.1" id="digital_x5F_marketing" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 128 128" fill="currentColor" class="text-white w-4 mr-3" xml:space="preserve">
+                    <path id="icon:4" class="st1" d="M74,35.3v12.5h21.6v23.6H74c0,26.4,0,56.6,0,56.6H50.3c0,0,0-27.5,0-56.6H30.4V47.8h19.9V30.6
+		c0-36.2,47.3-30.3,47.3-30.3v22C97.6,22.4,74,19.5,74,35.3z"/>
+</svg>
+            <span><?php _e('Via Facebook einloggen', 'ir21') ?></span>
         </a>
     </div>
     <div class="my-5 w-full">
         <a href="<?php echo $socialite->create('google')->redirect(); ?>"
-           class="bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
+           class="flex justify-center items-center bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
         >
-            <?php _e('Via Google einloggen', 'ir21') ?>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              fill="currentColor" class="text-white h-4  w-4 mr-3"   viewBox="0 0 128 128" xml:space="preserve">
+    <path id="icon_5_" d="M30.418,49.137
+		c-9.242-7.272-16.109-12.661-21.237-16.601C20.161,13.095,40.993,0,64.895,0c16.572,0,31.696,6.316,43.053,16.659
+		C94.563,29.32,94.997,30.044,89.724,35.317c-6.577-5.447-13.733-9.329-24.829-9.329C49.163,25.988,36.009,35.52,30.418,49.137z
+		 M27.491,64c0-1.97,0.145-3.882,0.435-5.765C20.48,52.382,12.107,45.892,5.299,40.677C2.459,47.891,0.895,55.772,0.895,64
+		c0,9.271,1.97,18.079,5.505,26.017c6.577-5.592,14.399-12.284,21.787-18.6C27.723,69.012,27.491,66.549,27.491,64z M64.895,102.012
+		c-15.124,0-27.871-8.837-33.811-21.613c-8.924,7.62-15.529,13.269-20.484,17.47C21.9,115.976,41.978,128,64.895,128
+		c15.326,0,27.321-3.882,36.476-10.169c-6.055-5.244-13.791-11.879-21.15-18.108C76.02,101.23,70.979,102.012,64.895,102.012z
+		 M126.345,51.339c-12.516,0-61.45,0-61.45,0v25.322c0,0,24.569-0.029,34.593-0.029c-2.665,7.996-5.997,14.255-11.183,18.513
+		c8.894,7.533,15.326,13.009,19.962,17.065C124.926,96.072,129.04,70.664,126.345,51.339z"/>
+</svg>
+            <span><?php _e('Via Google einloggen', 'ir21') ?></span>
         </a>
     </div>
 </div>
 <div class="py-2">
     <p class="font-medium"><?php _e('Noch keinen Account?', 'ir21') ?></p>
-    <p><a href="<?php echo get_field('field_601bc00528968', 'option') ?>" class="text-primary-100 underline"><?php _e('Hier registrieren', 'ir21') ?></a></p>
+    <p>
+        <a href="<?php echo get_field('field_601bc00528968', 'option') ?>" class="text-primary-100 underline"><?php _e('Hier registrieren', 'ir21') ?></a>
+    </p>
 </div>
--- page-templates/modal-immolive.php
+++ page-templates/modal-immolive.php
@@ -1,5 +1,7 @@
 <?php
+
 use Overtrue\Socialite\SocialiteManager;
+
 ?>
 
 <div x-data="{ show: false, user : false, id : false }"
@@ -63,7 +65,7 @@ use Overtrue\Socialite\SocialiteManager;
 
                         .</p>
                     <div class="grid grid-cols-2 gap-4">
-                        <div class="col-span-2 xl:col-span-1">
+                        <div class="col-span-2">
 
                             <?php
                             $ref = $_GET['ref'] ?? 'none';
@@ -74,7 +76,6 @@ use Overtrue\Socialite\SocialiteManager;
                                 <?php _e('E-Mail login', 'ir21') ?>
                             </a>
                         </div>
-                        <div class="col-span-2 xl:col-span-1">
                             <?php
                             $config = [
                                 'facebook' => [
@@ -82,16 +83,47 @@ use Overtrue\Socialite\SocialiteManager;
                                     'client_secret' => 'd6d52d59ce1f1efdbf997b980dffe229',
                                     'redirect'      => home_url('fb-login'),
                                 ],
+                                'google'   => [
+                                    'client_id'     => '194317471061-jdtvke2dpcensj3p9ckfq20cbsre23dl.apps.googleusercontent.com',
+                                    'client_secret' => 'O_JXIOXqatwxOMYq45ggJ1tj',
+                                    'redirect'      => home_url('g-oauth'),
+                                ],
                             ];
 
                             $socialite = new SocialiteManager($config);
                             ?>
 
-                            <a href="<?php echo $socialite->create('facebook')->withState($redirect)->redirect(); ?>"
-                               class="block bg-white text-primary-100 border border-primary-100 font-semibold text-center shadow-xl py-3 my-5 text-lg focus:outline-none focus:shadow-outline w-full text-center cursor-pointer"
-                            >
-                                <?php _e('Facebook login', 'ir21') ?>
-                            </a>
+                            <div>
+                                <a href="<?php echo $socialite->create('facebook')->redirect(); ?>"
+                                   class="flex justify-center items-center bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
+                                >
+                                    <svg version="1.1" id="digital_x5F_marketing" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                         viewBox="0 0 128 128" fill="currentColor" class="text-white w-4 mr-3" xml:space="preserve">
+                    <path id="icon:4" class="st1" d="M74,35.3v12.5h21.6v23.6H74c0,26.4,0,56.6,0,56.6H50.3c0,0,0-27.5,0-56.6H30.4V47.8h19.9V30.6
+		c0-36.2,47.3-30.3,47.3-30.3v22C97.6,22.4,74,19.5,74,35.3z"/>
+</svg>
+                                    <span><?php _e('Via Facebook einloggen', 'ir21') ?></span>
+                                </a>
+                            </div>
+                            <div>
+                                <a href="<?php echo $socialite->create('google')->redirect(); ?>"
+                                   class="flex justify-center items-center bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
+                                >
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                         fill="currentColor" class="text-white h-4  w-4 mr-3" viewBox="0 0 128 128" xml:space="preserve">
+    <path id="icon_5_" d="M30.418,49.137
+		c-9.242-7.272-16.109-12.661-21.237-16.601C20.161,13.095,40.993,0,64.895,0c16.572,0,31.696,6.316,43.053,16.659
+		C94.563,29.32,94.997,30.044,89.724,35.317c-6.577-5.447-13.733-9.329-24.829-9.329C49.163,25.988,36.009,35.52,30.418,49.137z
+		 M27.491,64c0-1.97,0.145-3.882,0.435-5.765C20.48,52.382,12.107,45.892,5.299,40.677C2.459,47.891,0.895,55.772,0.895,64
+		c0,9.271,1.97,18.079,5.505,26.017c6.577-5.592,14.399-12.284,21.787-18.6C27.723,69.012,27.491,66.549,27.491,64z M64.895,102.012
+		c-15.124,0-27.871-8.837-33.811-21.613c-8.924,7.62-15.529,13.269-20.484,17.47C21.9,115.976,41.978,128,64.895,128
+		c15.326,0,27.321-3.882,36.476-10.169c-6.055-5.244-13.791-11.879-21.15-18.108C76.02,101.23,70.979,102.012,64.895,102.012z
+		 M126.345,51.339c-12.516,0-61.45,0-61.45,0v25.322c0,0,24.569-0.029,34.593-0.029c-2.665,7.996-5.997,14.255-11.183,18.513
+		c8.894,7.533,15.326,13.009,19.962,17.065C124.926,96.072,129.04,70.664,126.345,51.339z"/>
+</svg>
+                                    <span><?php _e('Via Google einloggen', 'ir21') ?></span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
