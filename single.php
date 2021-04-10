eb3afb1b5ad7225150bf59148a215bcfa8e705e6
Gerhard Popp <gerhrad@poppgerhard.at>
2021-04-10 11:04:32 +0200

    wip

--- single.php
+++ single.php
@@ -2,12 +2,6 @@
 get_header();
 the_post();
 
-?>
-<div class="mt-64"></div>
-<a href="#comments" class="mt-64">kommentieren</a>
-
-
-<?php
 
 if (!get_post_format()) {
     get_template_part('page-templates/content', 'article');
