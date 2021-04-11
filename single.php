4f539311d340761aea8fb341c810547f8e7324db
Gerhard Popp <gerhrad@poppgerhard.at>
2021-04-11 20:55:53 +0200

    update article live Template

--- single.php
+++ single.php
@@ -5,7 +5,10 @@ the_post();
 $cats = wp_get_post_categories(get_the_ID());
 
 if(!empty(array_filter($cats, function ($cat){
-    if(in_array($cat, get_field('field_60733fe611fac', 'option') ?? [])){
+    $filter = get_field('field_60733fe611fac', 'option');
+    $filter = is_array($filter) ? $filter : [];
+
+    if(in_array($cat, $filter )){
         return $cat;
     }
 }))){
