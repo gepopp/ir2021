ac0d9b8a30ec01da9af3ec1959420f4877f9025d
Gerhard Popp <gerhrad@poppgerhard.at>
2021-05-06 22:43:36 +0200

    vimeo ga tag

--- pagetemplate-facook-login.php
+++ pagetemplate-facook-login.php
@@ -6,39 +6,39 @@
 session_start();
 
 
-$fb = new Facebook\Facebook([
+$fb = new Facebook\Facebook( [
 	'app_id'                => '831950683917414',
 	'app_secret'            => 'd6d52d59ce1f1efdbf997b980dffe229',
 	'default_graph_version' => 'v2.10',
-]);
+] );
 
 $helper = $fb->getRedirectLoginHelper();
 
-if (isset($_GET['state'])) {
-	$helper->getPersistentDataHandler()->set('state', $_GET['state']);
+if ( isset( $_GET['state'] ) ) {
+	$helper->getPersistentDataHandler()->set( 'state', $_GET['state'] );
 }
 
 try {
 	$accessToken = $helper->getAccessToken();
-} catch(Facebook\Exception\ResponseException $e) {
+} catch ( Facebook\Exception\ResponseException $e ) {
 	// When Graph returns an error
 	echo 'Graph returned an error: ' . $e->getMessage();
 	exit;
-} catch(Facebook\Exception\SDKException $e) {
+} catch ( Facebook\Exception\SDKException $e ) {
 	// When validation fails or other local issues
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
 	exit;
 }
 
-if (! isset($accessToken)) {
-	if ($helper->getError()) {
-		header('HTTP/1.0 401 Unauthorized');
+if ( ! isset( $accessToken ) ) {
+	if ( $helper->getError() ) {
+		header( 'HTTP/1.0 401 Unauthorized' );
 		echo "Error: " . $helper->getError() . "\n";
 		echo "Error Code: " . $helper->getErrorCode() . "\n";
 		echo "Error Reason: " . $helper->getErrorReason() . "\n";
 		echo "Error Description: " . $helper->getErrorDescription() . "\n";
 	} else {
-		header('HTTP/1.0 400 Bad Request');
+		header( 'HTTP/1.0 400 Bad Request' );
 		echo 'Bad request';
 	}
 	exit;
@@ -46,82 +46,80 @@ if (! isset($accessToken)) {
 
 // Logged in
 echo '<h3>Access Token</h3>';
-var_dump($accessToken->getValue());
+var_dump( $accessToken->getValue() );
 
 // The OAuth 2.0 client handler helps us manage access tokens
 $oAuth2Client = $fb->getOAuth2Client();
 
 // Get the access token metadata from /debug_token
-$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+$tokenMetadata = $oAuth2Client->debugToken( $accessToken );
 echo '<h3>Metadata</h3>';
-var_dump($tokenMetadata);
+var_dump( $tokenMetadata );
 
 // Validation (these will throw FacebookSDKException's when they fail)
-$tokenMetadata->validateAppId('831950683917414');
+$tokenMetadata->validateAppId( '831950683917414' );
 // If you know the user ID this access token belongs to, you can validate it here
 //$tokenMetadata->validateUserId('123');
 $tokenMetadata->validateExpiration();
 
-if (! $accessToken->isLongLived()) {
+if ( ! $accessToken->isLongLived() ) {
 	// Exchanges a short-lived access token for a long-lived one
 	try {
-		$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-	} catch (Facebook\Exception\SDKException $e) {
+		$accessToken = $oAuth2Client->getLongLivedAccessToken( $accessToken );
+	} catch ( Facebook\Exception\SDKException $e ) {
 		echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
 		exit;
 	}
 
 	echo '<h3>Long-lived</h3>';
-	var_dump($accessToken->getValue());
+	var_dump( $accessToken->getValue() );
 }
 
 try {
 	// Returns a `Facebook\Response` object
-	$response = $fb->get('/me?fields=id,name,email', $accessToken->getValue());
-} catch(Facebook\Exception\ResponseException $e) {
+	$response = $fb->get( '/me?fields=id,name,email', $accessToken->getValue() );
+} catch ( Facebook\Exception\ResponseException $e ) {
 	echo 'Graph returned an error: ' . $e->getMessage();
 	exit;
-} catch(Facebook\Exception\SDKException $e) {
+} catch ( Facebook\Exception\SDKException $e ) {
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
 	exit;
 }
 
 $fbuser = $response->getGraphUser();
 
-$user = get_user_by('email', $fbuser['email']);
+$user = get_user_by( 'email', $fbuser['email'] );
 
-if (!$user) {
+if ( ! $user ) {
 
-	$wp_user = wp_create_user($fbuser['name'] . ' ' . uniqid(), uniqid(), $fbuser['email']);
+	$wp_user = wp_create_user( $fbuser['name'] . ' ' . uniqid(), uniqid(), $fbuser['email'] );
 
-	wp_update_user([
+	wp_update_user( [
 		'ID'           => $wp_user,
 		'display_name' => $fbuser['name'],
-	]);
+	] );
 
-	(new CampaignMonitor())->transactional('registration_activated', $wp_user);
+	( new CampaignMonitor() )->transactional( 'registration_activated', $wp_user );
 
-	$user = get_user_by('ID', $wp_user);
+	$user = get_user_by( 'ID', $wp_user );
 
 
 }
 
 wp_clear_auth_cookie();
-wp_set_current_user($user->ID);
-wp_set_auth_cookie($user->ID);
+wp_set_current_user( $user->ID );
+wp_set_auth_cookie( $user->ID );
 
 
-if(!empty($_GET['redirect'])){
+if ( ! empty( $_GET['state'] ) ) {
 
-    $decoded = base64_decode($_GET['redirect']);
-    if(!$decoded){
-        $decoded = sanitize_text_field($_GET['redirect']);
-    }
-
-    wp_redirect(urldecode_deep($decoded));
-    exit();
+	$decoded = base64_decode( $_GET['state'] );
+	if ( get_page_by_path( basename( untrailingslashit( $decoded ) ), OBJECT ))
+	{
+		wp_redirect( urldecode_deep( $decoded ) );
+		exit();
+	}
 }
-
-wp_safe_redirect(get_field('field_601bc4580a4fc', 'option'));
+wp_safe_redirect( get_field( 'field_601bc4580a4fc', 'option' ) );
 exit();
 
