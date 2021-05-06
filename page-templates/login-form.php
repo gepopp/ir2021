854f4c06a47d735736be22bb83af6f225b6049da
Gerhard Popp <gerhrad@poppgerhard.at>
2021-05-06 21:10:01 +0200

    vimeo ga tag

--- page-templates/login-form.php
+++ page-templates/login-form.php
@@ -100,7 +100,7 @@ $socialite = new SocialiteManager($config);
         </a>
     </div>
     <div class="my-5 w-full">
-        <a href="<?php echo $socialite->create('google')->withState(urlencode($_GET['redirect'] ?? ''))->redirect(); ?>"
+        <a href="<?php echo $socialite->create('google')->redirect(); ?>"
            class="flex justify-center items-center bg-primary-100 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-center block"
         >
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
