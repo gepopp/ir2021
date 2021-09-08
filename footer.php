0bbb5ddb75c82c2edc251c88055fe440f067bc89
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-08 17:57:16 +0200

    remive immolive announcment

--- footer.php
+++ footer.php
@@ -52,5 +52,6 @@ mt-auto
     </div>
 </footer>
 <?php wp_footer(); ?>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -11,86 +11,91 @@ namespace immobilien_redaktion_2020;
 get_header();
 
 
-$query = new \WP_Query([
-    'post_type'           => 'post',
-    'post_status'         => 'publish',
-    'ignore_sticky_posts' => true,
-    'posts_per_page'      => 2,
-    'category__not_in'    => [696, 996, 159],
-]);
+$query = new \WP_Query( [
+	'post_type'           => 'post',
+	'post_status'         => 'publish',
+	'ignore_sticky_posts' => true,
+	'posts_per_page'      => 2,
+	'category__not_in'    => [ 696, 996, 159 ],
+] );
 ?>
 
 
     <div class="container mx-auto mt-20 relative px-5 lg:px-0">
         <div class="grid grid-cols-2 gap-10">
-            <?php if ($query->have_posts()): ?>
-                <?php while ($query->have_posts()): ?>
-                    <?php $query->the_post(); ?>
+			<?php if ( $query->have_posts() ): ?>
+				<?php while ( $query->have_posts() ): ?>
+					<?php $query->the_post(); ?>
                     <div class="col-span-2 md:col-span-1 relative">
                         <div class="col-span-2 md:col-span-1 relative">
                             <a href="<?php the_permalink(); ?>" class="relative block bg-primary-100 h-full image-holder">
-                                <?php the_post_thumbnail('article', ['class' => 'w-full h-auto max-w-full', 'onerror' => "this.style.display='none'"]); ?>
-                                <?php get_template_part('page-templates/snippet', 'heading') ?>
+								<?php the_post_thumbnail( 'article', [ 'class'   => 'w-full h-auto max-w-full',
+								                                       'onerror' => "this.style.display='none'",
+								] ); ?>
+								<?php get_template_part( 'page-templates/snippet', 'heading' ) ?>
                             </a>
                         </div>
                     </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
         </div>
-        <?php wp_reset_postdata(); ?>
+		<?php wp_reset_postdata(); ?>
     </div>
 
 
-    <?php get_template_part('banner-templates/banner', 'mega') ?>
+	<?php get_template_part( 'banner-templates/banner', 'mega' ) ?>
 
 
-<!--announcement-->
-<!--    --><?php //get_template_part('page-templates/page', 'immoliveAnnouncement') ?>
+    <!--announcement-->
+    <!--    --><?php //get_template_part('page-templates/page', 'immoliveAnnouncement')
+?>
 
-    <?php get_template_part('banner-templates/banner', 'fourbanner') ?>
+	<?php get_template_part( 'banner-templates/banner', 'fourbanner' ) ?>
 
-    <?php get_template_part('page-templates/part', 'aktuelles') ?>
+	<?php get_template_part( 'page-templates/part', 'aktuelles' ) ?>
 
-    <?php get_template_part('banner-templates/banner', 'thirds2') ?>
+	<?php get_template_part( 'banner-templates/banner', 'thirds2' ) ?>
 
-    <?php
+	<?php
 
-        $query = new \WP_Query([
-            'post_type'           => 'post',
-            'post_status'         => 'publish',
-            'ignore_sticky_posts' => true,
-            'posts_per_page'      => 2,
-            'offset'              => 2,
-            'category__not_in'    => [696, 996, 159],
-        ]);
+$query = new \WP_Query( [
+	'post_type'           => 'post',
+	'post_status'         => 'publish',
+	'ignore_sticky_posts' => true,
+	'posts_per_page'      => 2,
+	'offset'              => 2,
+	'category__not_in'    => [ 696, 996, 159 ],
+] );
 ?>
 
 
     <div class="container mx-auto mt-20 relative px-5 lg:px-0">
 
         <div class="grid grid-cols-2 gap-10">
-            <?php if ($query->have_posts()): ?>
-                <?php while ($query->have_posts()): ?>
-                    <?php $query->the_post(); ?>
+			<?php if ( $query->have_posts() ): ?>
+				<?php while ( $query->have_posts() ): ?>
+					<?php $query->the_post(); ?>
                     <div class="col-span-2 md:col-span-1 relative">
                         <div class="col-span-2 md:col-span-1 relative">
                             <a href="<?php the_permalink(); ?>" class="relative block bg-primary-100 h-full image-holder">
-                                <?php the_post_thumbnail('article', ['class' => 'w-full h-auto max-w-full', 'onerror' => "this.style.display='none'"]); ?>
-                                <?php get_template_part('page-templates/snippet', 'heading') ?>
+								<?php the_post_thumbnail( 'article', [ 'class'   => 'w-full h-auto max-w-full',
+								                                       'onerror' => "this.style.display='none'",
+								] ); ?>
+								<?php get_template_part( 'page-templates/snippet', 'heading' ) ?>
                             </a>
                         </div>
                     </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
         </div>
-        <?php wp_reset_postdata(); ?>
+		<?php wp_reset_postdata(); ?>
 
 
     </div>
 
 
-    <?php
-get_template_part('page-templates/modal', 'immolive');
+	<?php
+get_template_part( 'page-templates/modal', 'immolive' );
 
 
 get_footer();
