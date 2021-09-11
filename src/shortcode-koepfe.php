5c1fda1f69bb3025cf7393ead0fe8c2d98962159
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-11 09:12:23 +0200

    seperte content types

--- src/shortcode-koepfe.php
+++ src/shortcode-koepfe.php
@@ -25,9 +25,9 @@ add_shortcode( 'koepfe', function ( $atts = [] ) {
 	];
 
 	if ( ! empty( $ids ) ) {
-		$arg['post__in'] = explode(',', $ids);
+		$arg['post__in'] = explode( ',', $ids );
 		$arg['orderby']  = 'post__in';
-	}else{
+	} else {
 		$arg['posts_per_page'] = 5;
 	}
 
@@ -59,7 +59,7 @@ add_shortcode( 'koepfe', function ( $atts = [] ) {
                             <figure>
 								<?php
 								echo get_the_post_thumbnail( get_the_ID(), '430x258true', [ 'class' => 'w-full h-auto' ] );
-								$caption = wp_get_attachment_caption( get_post_thumbnail_id(get_the_ID()) );
+								$caption = wp_get_attachment_caption( get_post_thumbnail_id( get_the_ID() ) );
 								if ( ! empty( $caption ) ):
 									?>
                                     <figcaption>
@@ -68,11 +68,11 @@ add_shortcode( 'koepfe', function ( $atts = [] ) {
 								<?php endif; ?>
                             </figure>
                         </div>
-
+                        <div class="lg:pr-10 lg:pl-10 justify-end justify-start"></div>
                         <div class="h-full text-primary-100 w-full  lg:w-2/3 <?php echo $layout == 'right' || ( $layout == 'alternate' && $runner % 2 == 0 ) ? 'lg:pr-10' : 'lg:pl-10' ?> pt-10 lg:pt-0">
                             <div class="flex flex-col">
-                                <h3 class="text-3xl mb-0"><?php the_field( 'field_613c53f33d6b8' ) ?> <?php the_field( 'field_613b8ca49b06b' ) ?></h3>
-                                <p class="text-sm italic mb-5"><?php the_field( 'field_613c54063d6b9' );  ?> bei <?php the_field( 'field_613b8caa9b06c' ); ?></p>
+                                <h3 class="text-3xl mb-0"><?php the_field( 'field_613c53f33d6b8' ) ?><?php the_field( 'field_613b8ca49b06b' ) ?></h3>
+                                <p class="text-sm italic mb-5"><?php the_field( 'field_613c54063d6b9' ); ?> bei <?php the_field( 'field_613b8caa9b06c' ); ?></p>
                                 <p class="line-clamp-3 flex-1 text-gray-900"><?php echo get_the_excerpt(); ?></p>
                             </div>
                         </div>
