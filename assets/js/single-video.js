efc678859e916b6c867b5a02647ab5432c9dde2b
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-13 12:10:09 +0200

    bugfix

--- assets/js/single-video.js
+++ assets/js/single-video.js
@@ -40,7 +40,6 @@ window.prerolled = function (main_id, preroll, skip) {
                         this.prerollPlayer.on('playing', () => this.isPlaying = true);
                         this.prerollPlayer.on('loaded', () => {
                             this.isLoaded = true;
-                            this.prerollPlayer.play();
                         });
                     }else{
                         this.isPreroll = false;
