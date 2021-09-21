44b31cbb2f883119b26e2e8a5e656880d7533f89
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-21 20:32:17 +0200

    immolive subscribe and content lock

--- classes/Boot/ACF.php
+++ classes/Boot/ACF.php
@@ -20,6 +20,7 @@ class ACF {
 
 	}
 
+
 	public function create_ics_datei( $value, $post_id, $field, $original ) {
 
 		if ( ! empty( $value ) ) {
@@ -29,8 +30,8 @@ class ACF {
 		$starts = new \Carbon\Carbon( get_field( 'field_5ed527e9c2279', $post_id, false ), 'Europe/Vienna' );
 		$starts->setTimezone( 'UTC' );
 
-		$start = $starts->format('Ymd\THis\Z');
-		$end = $starts->addHour()->format('Ymd\THis');
+		$start = $starts->format( 'Ymd\THis\Z' );
+		$end   = $starts->addHour()->format( 'Ymd\THis' );
 
 
 		$ics = new \irclasses\ICS( [
@@ -40,7 +41,7 @@ class ACF {
 			'dtend'       => $end,
 			'summary'     => get_the_title( $post_id ),
 			'url'         => get_the_permalink( $post_id ),
-			'vtimezone'   => 'Europe/Vienna'
+			'vtimezone'   => 'Europe/Vienna',
 		] );
 
 		$template_dir = immobilien_redaktion_2020_DIR . '/tmp/';
@@ -54,7 +55,7 @@ class ACF {
 		$wordpress_upload_dir = wp_upload_dir();
 		$new_file_path        = $wordpress_upload_dir['path'] . '/' . $post_id . '.ics';
 
-		rename($filename, $new_file_path);
+		rename( $filename, $new_file_path );
 
 		$upload_id = wp_insert_attachment( [
 			'guid'           => $new_file_path,
@@ -65,7 +66,9 @@ class ACF {
 		], $new_file_path );
 
 		require_once( ABSPATH . 'wp-admin/includes/image.php' );
-		wp_update_attachment_metadata( $upload_id, wp_generate_attachment_metadata( $upload_id, $new_file_path ) );
+
+		$metadata = wp_generate_attachment_metadata( $upload_id, $new_file_path );
+		wp_update_attachment_metadata( $upload_id,  $metadata );
 
 		return $upload_id;
 
