cf9e5be148fb540e5d3695ede69b9a4ba45ef1a0
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-21 14:18:53 +0200

    immolive subscribe and content lock

--- templates/immolive/subscribeform.php
+++ templates/immolive/subscribeform.php
@@ -1,7 +1,7 @@
 <div class="pt-5">
     <h3 class="font-semibold text-primary-100 font-serif text-xl text-center">Zu diesem Livestream anmelden</h3>
     <p class="text-sm my-3">
-        Um bei unseren Podiumsdiskussionen uns Interviews Live dabei sein zu könen ist eine Anmeldung erforderlich.
+        Um bei unseren Podiumsdiskussionen uns Interviews live dabei sein zu können ist eine Anmeldung erforderlich.
         Erstellen Sie jetzt Ihren Account auf der Immobilien Redaktion und melden Sie sich an.
     </p>
 
