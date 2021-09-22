6629f364faaa258d130d0eaa71271c5d5227d7b7
Gerhard Popp <gerhrad@poppgerhard.at>
2021-09-22 12:15:10 +0200

    add speaker to single immolive

--- taxonomy-aktuelles_category.php
+++ taxonomy-aktuelles_category.php
@@ -1,33 +1,32 @@
 <?php get_header();
-
-
+$term = get_queried_object();
 ?>
-    <!--
-		UPDATE wp_posts p
-		LEFT JOIN wp_term_relationships rel ON rel.object_id = p.ID
-		LEFT JOIN wp_term_taxonomy tax ON tax.term_taxonomy_id = rel.term_taxonomy_id
-		LEFT JOIN wp_terms t ON t.term_id = tax.term_id
-		SET p.post_type = "aktuelles"
-		WHERE p.post_type = "post"
-		AND t.name = 'Aktuelles'
-		AND tax.taxonomy = 'category'
-	-->
+
+
+    <div class="container mx-auto mt-20">
+        <h1 class="font-sans text-5xl uppercase font-semibold text-gray-800 text-center">
+            <a href="/lesen-2" class="underline">
+				<?php _e( 'lesen', 'ir21' ) ?>
+            </a>
+        </h1>
+    </div>
+
 <?php get_template_part( 'banner', 'mega' ) ?>
 
 
     <div class="container mx-auto mt-20 px-5 lg:px-0 relative" x-data="{show:0}" x-init="window.onload = () => show = 1; inter = setInterval( () => { if(show < 3) { show++; } else { clearInterval(inter); } }, 500 ) ">
         <div class="flex flex-col lg:flex-row items-end">
-            <div class="w-full lg:w-1/2 relative" style="background-color: <?php the_field( 'field_613b5990f3543', 'option' ); ?>"
+            <div class="w-full lg:w-1/2 relative" style="background-color: <?php the_field( 'field_5c63ff4b7a5fb', $term ); ?>"
                  x-show.transition.fade.in="show > 0"
                  x-cloak>
-                <p class="text-white font-serif text-5xl py-24 px-5 text-center">Aktuelles</p>
+                <p class="text-white font-serif text-5xl py-24 px-5 text-center"><?php echo $term->name ?></p>
 
-				<?php if ( get_field( 'field_613b59adf3545', 'option' ) ): ?>
+				<?php if ( get_field( 'field_60da235237ec4', $term ) ): ?>
                     <div class="flex flex-col lg:absolute lg:top-100 lg:-mt-20 right-0 z-50 lg:max-w-xs shadow-2xl" x-show.transition.fade="show >= 3" x-cloak>
                         <p class="text-white">powered by</p>
                         <div class="bg-white flex justify-center w-full">
-                            <a href="<?php echo get_field( 'field_613b5a844db76', 'option' ) ?>">
-                                <img src="<?php the_field( 'field_613b59adf3545', 'option' ) ?>" class="w-auto h-auto">
+                            <a href="<?php echo get_field('field_5f9aeff4efa16', $term) ?>">
+                                <img src="<?php the_field( 'field_60da235237ec4', $term ) ?>" class="w-auto h-auto">
                             </a>
                         </div>
                     </div>
@@ -35,7 +34,7 @@
 
             </div>
             <div class="w-full lg:w-1/2 bg-gray-900 text-white -ml-5 -mb-5 p-5 relative" x-show.transition.fade="show >= 2" x-cloak>
-				<?php the_field( 'field_613b59a0f3544', 'option' ); ?>
+				<?php echo $term->description ?>
             </div>
         </div>
 
@@ -81,8 +80,9 @@
 		<?php endif; ?>
     </div>
 
+
     <div class="mt-48">
-		<?php \irclasses\Pagination::paginate(); ?>
+		<?php \irclasses\Pagination::paginate();?>
     </div>
 
 
