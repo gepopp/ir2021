c733c4c9aea9ff9f915058b8aa292a588aba74d0
Gerhard Popp <gerhrad@poppgerhard.at>
2021-10-31 11:55:46 +0100

    debug

--- templates/article/iconbar.php
+++ templates/article/iconbar.php
@@ -201,9 +201,9 @@
                     <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z"></path>
                 </svg>
             </div>
-            <p class="text-sm">
-                <?php echo sprintf(__(' Wir senden Ihnen in 3 Tagen ein Erinnerungsmail. Sie können den Zeitpunkt dieses Mails in Ihrem <a href="%s" target="_blank" class="text-primary-100 underline">Userprofil</a> ändern..', get_field('field_601bc4580a4fc'))) ?>
-            </p>
+<!--            <p class="text-sm">-->
+<!--                --><?php //echo sprintf(__(' Wir senden Ihnen in 3 Tagen ein Erinnerungsmail. Sie können den Zeitpunkt dieses Mails in Ihrem <a href="%s" target="_blank" class="text-primary-100 underline">Userprofil</a> ändern..', get_field('field_601bc4580a4fc'))) ?>
+<!--            </p>-->
         </div>
 
 
