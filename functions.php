139102d3bfd2a0aafb9b1299b1229bd07359cde2
Gerhard Popp <gerhrad@poppgerhard.at>
2022-01-27 00:37:17 +0100

    update rest

--- functions.php
+++ functions.php
@@ -20,8 +20,7 @@ $loader->addPsr4('irclasses\\', __DIR__ . '/classes');
 new Boot();
 
 
-function maximum_api_filter($query_params) {
+add_filter('rest_page_collection_params', function ($query_params){
 	$query_params['per_page']["maximum"]=1000;
 	return $query_params;
-}
-add_filter('rest_page_collection_params', 'maximum_api_filter');
+});
