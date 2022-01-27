b19e1476a9eb95100fe764674dc2d52dcba5c6ab
Gerhard Popp <gerhrad@poppgerhard.at>
2022-01-27 01:08:57 +0100

    alter pre query

--- classes/Boot/Query.php
+++ classes/Boot/Query.php
@@ -12,17 +12,17 @@ class Query {
 	}
 
 
-	public function set_posts_per_page( $query ) {
+	public function set_posts_per_page( \WP_Query $query ) {
 
 		global $wp_the_query;
 
-		if ( $query->is_post_type_archive('zur_person') ) {
+		if ( is_post_type_archive('zur_person') ) {
 			$query->set( 'posts_per_page', 11 );
 			$query->set('meta_key', 'zur_person_name' );
 			$query->set('orderby', 'meta_value');
 			$query->set('order', 'ASC');
 		}
-		// Etc..
+
 
 		return $query;
 	}
