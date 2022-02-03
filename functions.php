862e50f1b328bc9375116b816870d881feed9e26
Gerhard Popp <gerhrad@poppgerhard.at>
2022-02-03 16:27:24 +0100

    rest

--- functions.php
+++ functions.php
@@ -64,8 +64,9 @@ add_action( 'rest_api_init', function () {
 });
 
 add_action( 'rest_api_init', function() {
-	register_rest_field( 'comment', 'authorEmail', array(
+	register_rest_field( 'comment', 'author_email', array(
 			'get_callback'    => function($comment) {
+				wp_die(var_dump($comment));
 				return get_comment_author_email($comment);
 			}
 		)
