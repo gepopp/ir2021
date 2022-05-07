73f50d3f36591b7d116cda73589d4e8ce5762151
Gerhard Popp <gerhrad@poppgerhard.at>
2022-05-07 20:40:08 +0200

    rest

--- functions.php
+++ functions.php
@@ -126,3 +126,14 @@ add_action( 'rest_api_init', function () {
 		},
 	) );
 } );
+
+
+add_action( 'init', 'my_book_cpt' );
+function my_book_cpt() {
+	$args = array(
+		'public'       => true,
+		'show_in_rest' => true,
+		'label'        => 'Aktuelles'
+	);
+	register_post_type( 'aktuelle_presse', $args );
+}
